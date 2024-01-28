878816f6e13078cb37c1730210489a1fefc6ad19
Menjanahary <menjaraz@gmail.com>
2024-01-28 16:33:58 +0300

    Update main_test.go

--- golang/main_test.go
+++ golang/main_test.go
@@ -12,17 +12,15 @@ func TestA165736(t *testing.T) {
 		}
 	}
 
-	assert(a165736(big.NewInt(1)).Cmp(big.NewInt(1)) == 0)
-	//
-	assert(a165736(big.NewInt(10)).Cmp(big.NewInt(0)) == 0)
-	assert(a165736(big.NewInt(20)).Cmp(big.NewInt(0)) == 0)
-	assert(a165736(big.NewInt(30)).Cmp(big.NewInt(0)) == 0)
-	assert(a165736(big.NewInt(40)).Cmp(big.NewInt(0)) == 0)
+	assert(a165736(big.NewInt(  1)).Cmp(big.NewInt(1)) == 0)
+	assert(a165736(big.NewInt(  6)).Cmp(big.NewInt(7447238656)) == 0)
+	assert(a165736(big.NewInt( 10)).Cmp(big.NewInt(0)) == 0)
+	assert(a165736(big.NewInt( 11)).Cmp(big.NewInt(9172666611)) == 0)
+	assert(a165736(big.NewInt( 16)).Cmp(big.NewInt(290415616)) == 0)
+	assert(a165736(big.NewInt( 19)).Cmp(big.NewInt(609963179)) == 0)
+	assert(a165736(big.NewInt( 20)).Cmp(big.NewInt(0)) == 0)
+	assert(a165736(big.NewInt( 23)).Cmp(big.NewInt(1075718247)) == 0)
+	assert(a165736(big.NewInt( 30)).Cmp(big.NewInt(0)) == 0)
+	assert(a165736(big.NewInt( 40)).Cmp(big.NewInt(0)) == 0)
 	assert(a165736(big.NewInt(100)).Cmp(big.NewInt(0)) == 0)
-	//
-	assert(a165736(big.NewInt(6)).Cmp(big.NewInt(7447238656)) == 0)
-	assert(a165736(big.NewInt(11)).Cmp(big.NewInt(9172666611)) == 0)
-	assert(a165736(big.NewInt(16)).Cmp(big.NewInt(290415616)) == 0)
-	assert(a165736(big.NewInt(19)).Cmp(big.NewInt(609963179)) == 0)
-	assert(a165736(big.NewInt(23)).Cmp(big.NewInt(1075718247)) == 0)
 }
